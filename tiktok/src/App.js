c035402eca8d4b4ca3d29ae1f3fee85bff7e2a5f
hongphuctb98 <you@example.com>
2023-06-19 04:58:09 +0700

    update

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const orders = [100, 200, 500];
+const gifts = ["cpu i9", "cpu i7", "cpu i5"];
+
+const course = [
+  { id: 1, name: "html,css" },
+  { id: 2, name: "js" },
+  { id: 3, name: "react" },
+];
+const tabs = ["posts", "photos", "albums"];
 
 function App() {
+  // const [gift, setGift] = useState();
+  // const handleUpdate = () => {
+  //   setGift(gifts[Math.floor(Math.random() * gifts.length)]);
+  // };
+  // return (
+  //   <div className="App">
+  //     <h1>{gift || "Chua co phan thuong"}</h1>
+  //     <button onClick={handleUpdate}>Increase</button>
+  //   </div>
+  // );
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const handleSubmit = () => {
+  //   console.log({
+  //     name,
+  //     email,
+  //   });
+  // };
+
+  const [title, setTitle] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [type, setType] = useState("posts");
+  const [show, setShow] = useState(false);
+
+  console.log(type);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then((res) => res.json())
+      .then((posts) => {
+        setPosts(posts);
+      });
+  }, [type]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShow(window.scrollY >= 200);
+    };
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {tabs.map((tab) => (
+        <button
+          style={
+            type === tab
+              ? { color: "#fff", backgroundColor: "#000" }
+              : { color: "#000", backgroundColor: "#fff" }
+          }
+          key={tab}
+          onClick={() => setType(tab)}
         >
-          Learn React
-        </a>
-      </header>
+          {tab}
+        </button>
+      ))}
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      ></input>
+      <ul>
+        {posts.map((posts) => (
+          <li key={posts.id}>{posts.title}</li>
+        ))}
+      </ul>
+
+      {show && (
+        <button style={{ position: "fixed", bottom: "10px", right: "10px" }}>
+          goto top
+        </button>
+      )}
     </div>
   );
 }
